33e18172b37d4f335a929c15e3c88214c349dc6b
Nadine <22122006+nadinetab@users.noreply.github.com>
2022-03-05 10:52:33 -0800

    Changed Bootswatch theme to LITERA

--- dashboard/index.py
+++ dashboard/index.py
@@ -2,5 +2,5 @@ from dash import Dash
 import dash_bootstrap_components as dbc
 
 
-app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+app = Dash(__name__, external_stylesheets=[dbc.themes.LITERA])
 app.title = "Runner's Dash"
