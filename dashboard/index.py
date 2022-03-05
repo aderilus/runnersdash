12bacde2a451c60c147fc4b75d479ba38e6f1938
Nadine <22122006+nadinetab@users.noreply.github.com>
2022-03-05 10:13:59 -0800

    Changed Bootstrap theme

--- dashboard/index.py
+++ dashboard/index.py
@@ -2,4 +2,5 @@ from dash import Dash
 import dash_bootstrap_components as dbc
 
 
-app = Dash(__name__, external_stylesheets=dbc.themes.LUX)
+app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+app.title = "Runner's Dash"
