8b69f259c58fba4c12ca23c40cacc8062c133c39
Nadine <22122006+nadinetab@users.noreply.github.com>
2022-03-05 10:14:20 -0800

    Filled in header text

---
+++ dashboard/layout/header.py
@@ -0,0 +1,10 @@
+from dash import html
+
+header = html.Div(
+    id="header",
+    className="dashheader",
+    children=[
+        html.H1("Runner's Dash"),
+        html.H6("View running statistics and other health metrics, \
+                from imported Apple Health data.")
+    ])
