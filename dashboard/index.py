c6e3beeced6fbe97dfa27c2b0e873e2fa4d9e3b5
Nadine <22122006+nadinetab@users.noreply.github.com>
2022-03-06 09:32:16 -0800

    Passes in app title in Dash object parameters)

--- dashboard/index.py
+++ dashboard/index.py
@@ -2,5 +2,6 @@ from dash import Dash
 import dash_bootstrap_components as dbc
 
 
-app = Dash(__name__, external_stylesheets=[dbc.themes.LITERA])
-app.title = "Runner's Dash"
+app = Dash(__name__,
+           title="Runner's Dash",
+           external_stylesheets=[dbc.themes.LITERA])
