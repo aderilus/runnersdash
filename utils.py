5ffea49ba154d218245a70c227236ebb86460ca2
Nadine <22122006+nadinetab@users.noreply.github.com>
2022-03-07 00:55:02 -0800

    Constants and routines used in data processing scripts and Dash app scripts

---
+++ utils.py
@@ -0,0 +1,8 @@
+""" Used by both preparedatasets.py and Dash app files.
+"""
+
+# --- Constants --- #
+DAYS_OF_WK = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+ABBREV_MONTHS = ['Jan', 'Feb', 'Mar', 'Apr',
+                 'May', 'Jun', 'Jul', 'Aug',
+                 'Sep', 'Oct', 'Nov', 'Dec']
