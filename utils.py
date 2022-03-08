f48323556f48cfc57ba883f4b247075d147b7e06
Nadine <22122006+nadinetab@users.noreply.github.com>
2022-03-07 21:10:25 -0800

    Added function to get the extrema of a year column

--- utils.py
+++ utils.py
@@ -1,8 +1,24 @@
 """ Used by both preparedatasets.py and Dash app files.
 """
 
+from numpy import sort
+
 # --- Constants --- #
 DAYS_OF_WK = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
 ABBREV_MONTHS = ['Jan', 'Feb', 'Mar', 'Apr',
                  'May', 'Jun', 'Jul', 'Aug',
                  'Sep', 'Oct', 'Nov', 'Dec']
+
+
+def get_year_extrema(df, year_column):
+    """ Returns the years of the oldest and newest entry of the
+        given DataFrame.
+    """
+
+    unique_years = df[year_column].unique()
+    sorted_years = sort(unique_years)
+
+    oldest = sorted_years[0]
+    newest = sorted_years[-1]
+
+    return oldest, newest
