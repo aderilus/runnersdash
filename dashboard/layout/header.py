2e669f4a7bdc803dd396dbd58fa0246b731c75e8
Nadine <22122006+nadinetab@users.noreply.github.com>
2022-03-10 01:57:13 -0800

    Changed from header to navbar.

--- dashboard/layout/header.py
+++ dashboard/layout/header.py
@@ -1,10 +1,20 @@
 from dash import html
+from utils import get_column_extremas, get_resampled_runs
+import dash_bootstrap_components as dbc
 
-header = html.Div(
-    id="header",
-    className="dashheader",
+runs_resampled = get_resampled_runs()
+min_year, max_year = get_column_extremas(runs_resampled, 'Year')
+
+navbar = dbc.NavbarSimple(
     children=[
-        html.H5("Runner's Dash"),
-        html.H6("View running statistics and other health metrics, \
-                from imported Apple Health data.")
-    ])
+        dbc.NavItem(dbc.NavLink("Home", href="/home"),
+                    id="home-link",),
+        dbc.NavItem(dbc.NavLink("Detailed view", href="/heatmapview"),
+                    id="heatmapview-link",),
+    ],
+    brand="Hello! 👋 Here are your running stats from {0} to {1}".format(min_year, max_year),
+    brand_href='/',
+    color="light",
+    dark=False,
+    style={"margin-bottom": "1.0rem"},
+)
