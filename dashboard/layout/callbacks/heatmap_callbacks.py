48a70d9783c29d59f46b80e70cc3301855a2683a
Nadine <22122006+nadinetab@users.noreply.github.com>
2022-03-10 02:03:03 -0800

    Only contains build_heatmap with Month in x-axis and Calendar Day in y-axis. Removed Day of the Week vs calendar day functionality.

--- dashboard/layout/callbacks/heatmap_callbacks.py
+++ dashboard/layout/callbacks/heatmap_callbacks.py
@@ -1,56 +1,34 @@
-""" Callbacks for heatmaps.
+""" Callback(s) for heatmap(s).
 """
+
 from dashboard.index import app
 from dashboard.layout.activityheatmap import build_heatmap
 from dash.dependencies import Input, Output
-from utils import DAYS_OF_WK, ABBREV_MONTHS
+from utils import ABBREV_MONTHS
 
 
 @app.callback(
     Output("monthbyday-heatmap", "figure"),
-    Input("heatmap-year", "value"),
-    Input("heatmap-zaxis", "value")
+    [Input("heatmap-year", "value"),
+     Input("heatmap-zaxis", "value")]
 )
 def update_monthbyday_heatmap(input_year, z):
 
-    # if heatmap_type == 'By Month':
     x = 'Month'
     y = 'Day'
     mapid = 'monthly-heatmap'
 
-    # figure layout parameters
-    w = 400
-    h = 700
-
-    # else:
-    #     x = 'Calendar Week'
-    #     y = 'Day of Week'
-    #     mapid = 'weekly-heatmap'
-
-    #     w = 700
-    #     h = 400
-
     fig = build_heatmap(input_year, x_col='Month', y_col='Day',
                         z_col=z, hmap_id=mapid)
     fig.update_layout(
-        width=w,
-        height=h,
-    )
-
-    # Format axes tick labels
-    # if heatmap_type == 'By Month':
-    fig.update_layout(
+        width=350,
+        height=650,
+        margin=dict(l=5, r=5, b=5, t=20),
         xaxis=dict(
             tickmode='array',
             tickvals=list(range(1, 13)),
             ticktext=ABBREV_MONTHS,
-        )
+        ),
     )
-    # else:
-    #     fig.update_layout(
-    #         yaxis={
-    #             'categoryarray': DAYS_OF_WK
-    #         }
-    #     )
 
     return fig
