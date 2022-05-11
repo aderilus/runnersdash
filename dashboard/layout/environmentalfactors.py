e8f07428f7d3777dd73d8c74fb7c48c6f67ded46
Nadine <22122006+nadinetab@users.noreply.github.com>
2022-05-11 02:57:22 -0700

    Transformed temp and humidity scatter plots to box plots, with temp and humidity binned

--- dashboard/layout/environmentalfactors.py
+++ dashboard/layout/environmentalfactors.py
@@ -1,6 +1,11 @@
+from audioop import avg
 from dash import dcc
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
+from plotly import colors
+from pandas import cut, IntervalIndex, Interval
+from numpy import arange
+from math import floor, ceil
 from dashboard.assets.custom_themes import custom_theme1
 from utils import (get_latest_daily_agg,
                    colmapper, get_unit_from_string,
@@ -44,12 +49,12 @@ def build_weather_factors_plot(y):
     # Filter
     dfiltered = data[(data[indoor_workout] < 1) & (data[avg_temp].notna())]
 
-    # Get y units
+    # Get units
     y_unit = get_unit_from_string(y)
     humid_unit = get_unit_from_string(avg_humid)
     temp_unit = get_unit_from_string(avg_temp)
 
-    # Figure
+    # Figure configs and definition
     subplot_heights = [0.33, 0.33, 0.33]
     vspace = 0.1
     fig = make_subplots(rows=3, cols=1,
@@ -57,17 +62,19 @@ def build_weather_factors_plot(y):
                         row_heights=subplot_heights,
                         )
 
-    # Scatter with y as z-dim
-    trace0_name = y.split(' (')[0]
-    trace0_hover = ["Date: %{customdata}",
-                    "Temp: %{y:.2f}" + f" {temp_unit}",
-                    "Humidity: %{x} " + f"{humid_unit}<br>",
-                    f"{trace0_name}: " + "%{text:.2f}" + f" {y_unit}"
-                    ]
+    # Trace configs
+    trace_name = y.split(' (')[0]
+    trace_hover = ["Date: %{customdata}",
+                   "Temp: %{y:.2f}" + f" {temp_unit}",
+                   "Humidity: %{x} " + f"{humid_unit}<br>",
+                   f"{trace_name}: " + "%{text:.2f}" + f" {y_unit}"
+                   ]
+
+    # (x: Avg. Humidity, y: Avg. Temp, z: y parameter)
     fig.add_trace(
         go.Scatter(x=dfiltered[avg_humid],
                    y=dfiltered[avg_temp],
-                   name=trace0_name,
+                   name=trace_name,
                    mode='markers',
                    marker=dict(
                        size=7,
@@ -80,52 +87,89 @@ def build_weather_factors_plot(y):
                        showscale=True),
                    text=dfiltered[y],
                    customdata=dfiltered.index.date,
-                   hovertemplate=trace0_hover,
+                   hovertemplate=trace_hover,
+                   showlegend=False,
                    ),
         row=3, col=1,
     )
 
-    fig.add_trace(
-        go.Scatter(
-            x=dfiltered[avg_temp],
-            y=dfiltered[y],
-            name="",
-            mode='markers',
-            text=dfiltered[y],
-            customdata=dfiltered.index.date,
-            hovertemplate="<br>".join([trace0_hover[i] for i in [0, 1, 3]]),
-            # hovertemplate="<br>".join([
-            #     "Date: %{customdata}",
-            #     "Temp: %{x}" + f" {temp_unit}",
-            #     f"{trace0_name}: " + "%{y:.2f}" + f" {y_unit}",
-            # ]),
-        ),
-        row=1, col=1,
-    )
+    # Averaged y binned across temperature (x: Avg. temp, y: y parameter)
+    # The following DataFrame is used for both temp and humidity box plots
+    dbinned = dfiltered[[avg_temp, avg_humid, y]].copy()
 
-    fig.add_trace(
-        go.Scatter(
-            x=dfiltered[avg_humid],
-            y=dfiltered[y],
-            name="",
-            mode='markers',
-            text=dfiltered[y],
-            customdata=dfiltered.index.date,
-            hovertemplate="<br>".join([trace0_hover[i] for i in [0, 2, 3]]),
-        ),
-        row=2, col=1,
-    )
+    # Build temperature intervals
+    tintervals = f'Avg. Temp Interval ({temp_unit})'
+    t_delta = 5
+    tbin_start = floor(dbinned[avg_temp].min())
+    tbin_start -= tbin_start % 10
+    tbin_end = ceil(dbinned[avg_temp].max())
+    tbin_end += 10 - (tbin_end % 10)
+    tbins = arange(start=tbin_start,
+                   stop=tbin_end,
+                   step=t_delta,
+                   )
+    dbinned[tintervals] = cut(dbinned[avg_temp], bins=tbins, right=False)
+    dbinned['T_Left'] = IntervalIndex(dbinned[tintervals]).left
+    t_cscale = colors.sample_colorscale(colors.sequential.thermal,
+                                        samplepoints=len(tbins))
+    for idx, i in enumerate(tbins):
+        tfiltered = dbinned[dbinned['T_Left'] == i]
+        fig.add_trace(
+            go.Box(
+                x=tfiltered['T_Left'],
+                y=tfiltered[y],
+                name=f"[{i}, {i + 5}) {temp_unit}",
+                showlegend=False,
+                marker_color=t_cscale[idx],
+                hoverinfo='name+y',
+            ),
+            row=1, col=1
+        )
 
+    # Averaged y binned across humidity (x: Avg. Humidity, y: y param)
+    # Build humidity bins
+    hintervals = f"Avg. Humidity Interval ({humid_unit})"
+    hbin_start = floor(dbinned[avg_humid].min())
+    hbin_end = ceil(dbinned[avg_humid].max())
+    h_delta = 1000
+    hbins = arange(start=hbin_start,
+                   stop=hbin_end,
+                   step=h_delta)
+    dbinned[hintervals] = cut(dbinned[avg_humid], bins=hbins, right=False)
+    dbinned['H_Left'] = IntervalIndex(dbinned[hintervals]).left
+    for idx, j in enumerate(hbins):
+        hfiltered = dbinned[dbinned['H_Left'] == j]
+        fig.add_trace(
+            go.Box(
+                x=hfiltered['H_Left'],
+                y=hfiltered[y],
+                name=f"[{j}, {j + h_delta}) {humid_unit}",
+                showlegend=False,
+                marker_color='#6AACB9',
+                hoverinfo='name+y',
+            ),
+            row=2, col=1,
+        )
+
+    # Axis configs
+    x2_ticktext = [str(i) for i in hbins]
+    x2_ticktext[0] = f"[{hbins[0]},<br>{hbins[0] + h_delta})"
     fig.update_layout(
         template=custom_theme1,
-        height=700,
-        showlegend=False,
+        height=800,
+        xaxis=dict(
+            title_text=f'Avg. temp interval ({temp_unit}), ΔT = {t_delta}',
+            tickmode='array',
+            tickvals=tbins,
+            ticktext=[f"[{i}, {i + t_delta})" for i in tbins]),
+        yaxis=dict(title_text=y),
+        xaxis2=dict(title_text=f'Avg. humidity interval ({humid_unit}), ΔH = {h_delta}',
+                    tickmode='array',
+                    tickvals=hbins,
+                    ticktext=x2_ticktext),
+        yaxis2=dict(title_text=y),
         xaxis3=dict(title_text=avg_humid),
         yaxis3=dict(title_text=avg_temp),
-        xaxis=dict(title_text=avg_temp),
-        yaxis=dict(title_text=y),
-        xaxis2=dict(title_text=avg_humid),
-        yaxis2=dict(title_text=y)
     )
 
     return fig
